84d246fdfc9753bb407ac6b1df795f9ced8e79f6
Juan Pablo Díaz Correa <juanipis@gmail.com>
2024-10-14 14:21:49 -0500

    added frame motion

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,6 +2,7 @@
 import { ReactNode, useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { useRouter } from 'next/router';
+import { motion } from 'framer-motion'; // Import Framer Motion
 import UserDropdownMenu from './userDropdownMenu';
 import { SessionData } from '@/utils/withAuth';
 
@@ -13,7 +14,7 @@ interface LayoutProps {
 export default function Layout({ children, authData }: Readonly<LayoutProps>) {
   const router = useRouter();
   const currentRoute = router.pathname;
-  console.log(currentRoute);
+
   const [activeTab, setActiveTab] = useState<string>(currentRoute);
 
   const handleNavigation = (tab: string, path: string) => {
@@ -71,10 +72,17 @@ export default function Layout({ children, authData }: Readonly<LayoutProps>) {
           </div>
         </header>
 
-        {/* Page content */}
-        <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100">
+        {/* Page content with Framer Motion */}
+        <motion.main
+          key={currentRoute} // key based on the route for unique transitions
+          initial={{ opacity: 0, y: 20 }} // Initial state: slightly below with 0 opacity
+          animate={{ opacity: 1, y: 0 }} // Animate to: fully visible and in place
+          exit={{ opacity: 0, y: 20 }} // Exit state: fade out and slide down
+          transition={{ duration: 0.3 }} // Duration of transition
+          className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100"
+        >
           <div className="px-6 py-8">{children}</div>
-        </main>
+        </motion.main>
       </div>
     </div>
   );
