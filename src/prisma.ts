ff930d084afe512dfed5daff45b03e94cdbb58ac
Juan Pablo Díaz Correa <juanipis@gmail.com>
2024-10-15 13:44:42 -0500

    fix(types-prisma-cliet): fixed types prisma client

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,13 +1,18 @@
-import { PrismaClient } from '@prisma/client/edge'; // Asegúrate de usar el cliente Edge
+import { PrismaClient } from '@prisma/client/edge'; // Edge version
 import { withAccelerate } from '@prisma/extension-accelerate';
 
+// Define the type for the extended Prisma client
+type ExtendedPrismaClient = ReturnType<PrismaClient['$extends']>;
+
+// Define the global object
+// eslint-disable-next-line no-undef
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+  prisma: ExtendedPrismaClient | undefined;
 };
 
-// Crear un cliente Prisma Edge extendido con Accelerate
+// Create the extended Prisma client with Accelerate
 export const prisma =
   globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
 
-// Almacenar el cliente global solo en entornos no de producción
+// Store the client globally only in non-production environments
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
