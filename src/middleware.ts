52f3d019b6d506e134894c948e0dad966d72c727
Juan Pablo Díaz Correa <juanipis@gmail.com>
2024-10-15 13:28:00 -0500

    a bit

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,6 @@
 import { auth } from '@/auth';
 
 export default auth((req) => {
-  const path = req.nextUrl.pathname;
-  const auth = req.auth;
-  console.log('------------------------------------');
-  console.log('path', path);
-  console.log('auth', auth);
-  console.log('------------------------------------');
   if (!req.auth && req.nextUrl.pathname !== '/') {
     const newUrl = new URL('/', req.nextUrl.origin);
     return Response.redirect(newUrl);
