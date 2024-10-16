4a25aba3b4fb3c94906e33ad7ff0b29d79b78c66
Juan Pablo Díaz Correa <juanipis@gmail.com>
2024-10-15 20:47:58 -0500

    middleware protection

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,15 @@ export default auth((req) => {
     const newUrl = new URL('/', req.nextUrl.origin);
     return Response.redirect(newUrl);
   }
+
+  //protect /users, reports, if the user is not an admin
+  if (
+    req.auth &&
+    !req.auth.user.role.includes('admin') &&
+    req.nextUrl.pathname.startsWith('/users')
+  ) {
+    return new Response('Unauthorized', { status: 401 });
+  }
 });
 export const config = {
   matcher: ['/((?!api/(?!graphql)|_next/static|_next/image|favicon.ico).*)'],
